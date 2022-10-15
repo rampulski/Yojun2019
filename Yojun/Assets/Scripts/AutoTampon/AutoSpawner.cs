6ab1f9045b81e05c6910b58d4a50f1b4a914a987
Simon Chauvin <contact@simonchauvin.com>
2022-10-15 19:22:07 +0200

    Fixed spawn orientation.

--- Yojun/Assets/Scripts/AutoTampon/AutoSpawner.cs
+++ Yojun/Assets/Scripts/AutoTampon/AutoSpawner.cs
@@ -8,27 +8,29 @@ public class AutoSpawner : MonoBehaviour
     [SerializeField] private Transform center;
     [SerializeField] private Transform autoPrefab;
     [SerializeField] private int playerNumber;
+    [SerializeField] private float delay;
 
-    private bool spawnedAlready;
+    private Transform auto;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        spawnedAlready = false;
+        auto = null;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (inputManager.inputPlayersPressed[playerNumber - 1] &&!spawnedAlready)
+        if (inputManager.inputPlayersPressed[playerNumber - 1] && auto == null)
         {
-            Transform auto = Instantiate(autoPrefab, transform.position, Quaternion.Euler(0, 0, Mathf.Atan((center.position.y - transform.position.y) / (center.position.x - transform.position.x)) * Mathf.Rad2Deg), transform);
+            if (transform.position.x > center.position.x)
+                auto = Instantiate(autoPrefab, transform.position, Quaternion.Euler(0, 0, 180 - (Mathf.Asin((center.position.y - transform.position.y) / (center.position - transform.position).magnitude) * Mathf.Rad2Deg)), transform);
+            else
+                auto = Instantiate(autoPrefab, transform.position, Quaternion.Euler(0, 0, Mathf.Asin((center.position.y - transform.position.y) / (center.position - transform.position).magnitude) * Mathf.Rad2Deg), transform);
 
             if (auto.GetComponent<SkidBehaviour>())
-                auto.GetComponent<SkidBehaviour>().SetPlayerNumber(playerNumber - 1);
-
-            spawnedAlready = true;
+                auto.GetComponent<SkidBehaviour>().Init(playerNumber - 1, delay);
         }
     }
 }
