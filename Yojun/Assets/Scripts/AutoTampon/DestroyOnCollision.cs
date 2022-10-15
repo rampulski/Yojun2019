a43fe3cda3988f726f34c229a65173677bca4815
Simon Chauvin <contact@simonchauvin.com>
2022-10-15 19:25:25 +0200

    Border destroy auto and auto destroy when colliding with one another.

---
+++ Yojun/Assets/Scripts/AutoTampon/DestroyOnCollision.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnCollision : MonoBehaviour
+{
+    private MoveBehaviour moveBehaviour;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveBehaviour = GetComponent<MoveBehaviour>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (moveBehaviour.GetSpeed() < collision.gameObject.GetComponent<MoveBehaviour>().GetSpeed())
+            {
+                Destroy(gameObject);
+            }
+            if (moveBehaviour.GetSpeed() > collision.gameObject.GetComponent<MoveBehaviour>().GetSpeed())
+            {
+                Destroy(collision.gameObject);
+            }
+            {
+                Destroy(gameObject);
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+}
