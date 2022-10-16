b8e8bc88c955ba514808d6e6dc5ca2baee55ae73
Simon Chauvin <contact@simonchauvin.com>
2022-10-16 17:16:10 +0200

    Auto spawner.

--- Yojun/Assets/Scripts/Spirale/AutoSpawner.cs
+++ Yojun/Assets/Scripts/Spirale/AutoSpawner.cs
@@ -8,11 +8,17 @@ public class AutoSpawner : MonoBehaviour
     [SerializeField] private Transform center;
     [SerializeField] private Transform autoPrefab;
     [SerializeField] private int playerNumber;
-    [SerializeField] private float delay;
+    [SerializeField] private float delayToSpawn;
+    [SerializeField] private float delayToMove;
+    [SerializeField] private float delayToBan;
 
     private Transform auto;
 
     private int score;
+    private bool canSpawn;
+    private bool banned;
+    private float spawnTimer;
+    private float banTimer;
 
 
     void Start()
@@ -20,11 +26,36 @@ public class AutoSpawner : MonoBehaviour
         auto = null;
 
         score = 0;
+        canSpawn = false;
+        banned = true;
+        spawnTimer = 0;
+        banTimer = 0;
     }
 
     void Update()
     {
-        if (inputManager.inputPlayersPressed[playerNumber - 1] && auto == null)
+        if (inputManager.inputPlayersPressed[playerNumber - 1])
+        {
+            banTimer = 0;
+            banned = false;
+        }
+
+        banTimer += Time.deltaTime;
+        if (banTimer >= delayToBan)
+        {
+            banned = true;
+        }
+
+        if (auto == null && !canSpawn && !banned)
+        {
+            canSpawn = true;
+            spawnTimer = 0;
+        }
+
+        if (canSpawn)
+            spawnTimer += Time.deltaTime;
+
+        if (auto == null && !banned && canSpawn && spawnTimer >= delayToSpawn)
         {
             if (transform.position.x > center.position.x)
                 auto = Instantiate(autoPrefab, transform.position, Quaternion.Euler(0, 0, 180 - (Mathf.Asin((center.position.y - transform.position.y) / (center.position - transform.position).magnitude) * Mathf.Rad2Deg)), transform);
@@ -39,8 +70,10 @@ public class AutoSpawner : MonoBehaviour
             if (auto.GetComponent<TrailBehaviour>())
                 auto.GetComponent<TrailBehaviour>().Init(this, score);
 
-                if (auto.GetComponent<SkidBehaviour>())
-                auto.GetComponent<SkidBehaviour>().Init(playerNumber - 1, delay);
+            if (auto.GetComponent<SkidBehaviour>())
+                auto.GetComponent<SkidBehaviour>().Init(playerNumber - 1, delayToMove);
+
+            canSpawn = false;
         }
     }
 
